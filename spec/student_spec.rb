6e9aba525b7d9f09b781fafa0ed0ad35d5afac5b
maxwellbenton <maxwellbenton@gmail.com>
2019-07-12 11:46:02 -0400

    updates grade 10 test to be more specific

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -1,5 +1,5 @@
 require_relative 'spec_helper'
-
+require 'pry'
 describe Student do
 
   before do
@@ -142,9 +142,13 @@ describe Student do
         jess.name = "Jess"
         jess.grade = 10
         jess.save
-
+        
         first_X_students = Student.first_X_students_in_grade_10(2)
-        expect(first_X_students.size).to eq(2)
+        expect(first_X_students.size).to eq(2), 'Requested first 2 students in grade 10. Expected Array of two elements'
+        expect(first_X_students.all? {|student| student.class == Student}).to eq(true), 'Expected Array of Student instances'
+        first_X_students = Student.first_X_students_in_grade_10(3)
+        expect(first_X_students.size).to eq(3), 'Requested first 3 students in grade 10. Expected Array of three elements'
+        expect(first_X_students.all? {|student| student.class == Student}).to eq(true), 'Expected Array of Student instances'
       end
     end
 
