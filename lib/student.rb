378cf3f0fac9f2093076703fe4d9fac7720af034
Kierra M <k.merriman32@hotmail.com>
2019-11-02 00:34:41 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -90,7 +90,8 @@ class Student
   def self.first_X_students_in_grade_10(x)
     sql = "SELECT * FROM students WHERE grade = 10 LIMIT ?"
    DB[:conn].execute(sql, x).collect do |row|
-      self.new_from_db(row)
+      self.new_from_db(row) # this helpos create a new instance of student
+      binding.pry
   end
 end
   
